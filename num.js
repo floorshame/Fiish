7fef94023b177680463da3ae8be8cb5fa9cdeb50
https404 <hippostars@gmail.com>
2022-07-21 11:57:20 +1000

    i add stoff

---
+++ num.js
@@ -0,0 +1,21 @@
+//*AAAAAAAAAAA *//
+
+function shortennum(value) {
+    var placeholder = value;
+    if (value >= 1000) {
+        var suffixes = ["", "k", "m", "b","t"];
+        var suffixNum = Math.floor( (""+value).length/3 );
+        var shortValue = '';
+        for (var precision = 2; precision >= 1; precision--) {
+            shortValue = parseFloat( (suffixNum != 0 ? (value / Math.pow(1000,suffixNum) ) : value).toPrecision(precision));
+            var dotLessShortValue = (shortValue + '').replace(/[^a-zA-Z 0-9]+/g,'');
+            if (dotLessShortValue.length <= 2) { break; }
+        }
+        if (shortValue % 1 != 0)  shortValue = shortValue.toFixed(1);
+        placeholder = shortValue+suffixes[suffixNum];
+    }
+    console.log(placeholder);
+
+    return placeholder;
+    
+}
